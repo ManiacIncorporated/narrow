5bf7d3cc332c61d4e28eb76752ddb5cfe4ac89eb
02dtan <02dtan@gmail.com>
2025-07-16 23:18:27 +0000

    add 50% neuron/residual sparsity score distro

--- psych-llm/eval_suite/vis-bleurt-score.py
+++ psych-llm/eval_suite/vis-bleurt-score.py
@@ -8,7 +8,7 @@ def main(scorefile: str = None) -> None:
         with open(scorefile, 'rb') as f:
             scores = pickle.load(f)
         fig, ax = plt.subplots()
-        ax.hist(scores)
+        ax.hist(scores,bins=100)
         ax.set(xlabel="BLEURT score", ylabel="Frequency")
         ax.set_title("Distribution of BLEURT scores over 5k samples")
         fig.savefig('bleurtscores.png', dpi=300)
